a855e08a699d064991555650e4e27c7364f6ccee
Josef Fröhle <froehle@b1-systems.de>
2020-08-25 21:51:27 +0200

    Update main.go

--- cmd/xml2st/main.go
+++ cmd/xml2st/main.go
@@ -5,8 +5,8 @@ import "github.com/wxd237/xml2struct"
 import "os"
 
 func main() {
-	fmt.Printf("asfsafa")
-	res := xml2struct.Parserxml("../../document.xml")
-	restring := xml2struct.GenerateStruct(res, "Word", os.Stdout)
+	fmt.Printf("Start\n")
+	res := xml2struct.Parserxml("document.xml")
+	restring := xml2struct.GenerateStruct(res, "MyStruct", os.Stdout)
 	fmt.Print(restring)
 }
